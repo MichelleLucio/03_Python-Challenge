77b9437121c2a848e15ba019141230f1f473f6fc
Michelle Lucio <michellelucio2002@gmail.com>
2020-03-24 23:20:03 -0500

    commit homework changes

--- PyBank/ML_PyBank_main.py
+++ PyBank/ML_PyBank_main.py
@@ -7,27 +7,45 @@ import csv
 
 PyBank_csv_path = os.path.join('Resources','budget_data.csv')
 
+
+
+
 # Define the function and have it accept the 'PyBankInfo' as its sole parameter
-def budget(PyBankInfo):
+def budget():
+    PyBank_csv_path = os.path.join('Resources','budget_data.csv')
 
 #set variables
-    month = str(PyBankInfo[0])
-    Profit_Losses = int(PyBankInfo[1])
+    month = str(budget_data[0])   #problems with parameter here -needs definition!
+    Profit_Losses = int(budget_data[1])
 
 # Read the header row first (should this be here?)
     csv_header = next(csvfile)
-        print(f"CSV Header: {csv_header}")
+    print(f"CSV Header: {csv_header}")
 
 #count months
     Total_month = len(list(csvreader))
-
+    print(f"Total Months: {str(Total_month)}")
 #Net total of profit/losses over entire period
+    Sum_of_all = 0
+
+    for row in csvreader:
+       total += float(row[2])
+    print(f"Total: {str(Sum_of_all)}")
+
 
 #average of the changes in profit/losses over entire period
 
+
+
 #greatest increase in profits (date & amount) over entire period
+    maxNumber = max(Profit_Losses), 2
+    
+
 
 #greatest decrease in profits (date & amount) over entire period
+    minNumber = min(Profit_Losses), 2
+
+
 
 
 
@@ -37,6 +55,10 @@ def budget(PyBankInfo):
 with open(PyBank_csv_path,'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
+#print out analysis
+print ("Financial Analysis")
+print ("_____________________")
+budget()
 
 
 
