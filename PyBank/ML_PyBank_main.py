c58bdc89c66ff2e9672a9a853089a9093d9cb3d4
Michelle Lucio <michellelucio2002@gmail.com>
2020-03-26 12:35:20 -0500

    commit a copy of pybank while working on it

--- PyBank/ML_PyBank_main.py
+++ PyBank/ML_PyBank_main.py
@@ -7,101 +7,123 @@ import csv
 
 PyBank_csv_path = os.path.join('Resources','budget_data.csv')
 
+#with open(PyBank_csv_path,'r') as csvfile:
+    #csvreader = csv.reader(csvfile, delimiter=',')
 
-#PyBank_csv_path = os.path.join('Resources','budget_data.csv')
 
 #set variables
-    #month = str(PyBank_csv_path[0])   #problems with parameter here -needs definition!
-    #Profit_Losses = float(PyBank_csv_path[1])
-Sum_of_all = 0
-
-with open(PyBank_csv_path,'r') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-
-
-
-# Read the header row first (should this be here?)
-    csv_header = next(csvfile)
-    #print(f"CSV Header: {csv_header}")
-
-#count months
-    Total_month = len(list(csvreader))
-    print(f"Total Months: {str(Total_month)}")
-
-#Net total of profit/losses over entire period
-#profit_loss = int(row[1])
-#net_total = net_total + profit_loss
-
-    for row in csvreader:
-        #Sum_of_all = Sum_of_all + row[1]
-        Sum_of_all += row[1]
-    print("Total:  " + str(Sum_of_all))
+monthcounter = 0
+netamount = 0
+monthlydiff = 0
+monthlydifftotal = 0
+previousmonthamt = 0
+monthlydiffdict = {}
 
 
-#average of the changes in profit/losses over entire period
-
-
-
-#greatest increase in profits (date & amount) over entire period
-    maxNumber = (max(PyBank_csv_path), [1])
-    #print(f"Greatest Increase in Profits: "+ maxNumber)
-
-    #maxNumber = str(max(csvreader, 1))
-    #print("Greatest Increase in Profits:  + maxNumber[Profit_Losses[1]]+ maxNumber[Profit_Losses[2]]")
-    #print(f"Greatest Increase in Profits: + maxNumber")
-    print(maxNumber)
-
-#greatest decrease in profits (date & amount) over entire period
-    minNumber = (min(PyBank_csv_path), 1)
-
-    #minNumber = min(csvreader, int(Profit_Losses[2])
-    #print(f"Greatest Decrease in Profits:  {str(min(csvreader[1]))}")
-    print(minNumber)
-
-
-
-# Method 2: Improved Reading using CSV module
-#with open(csvpath, newline='') as csvfile:
-# CSV reader specifies delimiter and variable that holds contents
-with open(PyBank_csv_path,'r') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-
-#print out analysis
-print ("Financial Analysis")
-print ("_____________________")
-
-
-
-
-
-
-
-
-
-
-
-
-#test print
+#def loop for counting number of months
+def month_number():
+    PyBank_csv_path = os.path.join('Resources','budget_data.csv')
+    with open(PyBank_csv_path,'r') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',')
+    #monthcounter = 0
+        csv_header = next(csvfile)
+        monthcounter = len(list(csvreader))
     #for row in csvreader:
-        #if row[0] is ("Jan-2012"):
-            #print("This is working so far")
+        #if row [0] == "Date":
+            #pass
+        #else:
+            #counter = counter + 1
+            #print (row)
+            #if counter >= n:
+                #break
+    return monthcounter          
+
+
+#def loop for  net amount of profit/losses over entire period
+def net_total():
+    PyBank_csv_path = os.path.join('Resources','budget_data.csv')
+    with open(PyBank_csv_path,'r') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',')
+        netamount = 0
+        for row in csvreader:
+            if row [0] == "Date":
+                pass
+            else:
+                netamount = netamount + int(row[1])
+                #print (row)
+
+    return netamount 
+
+
+#def for creating a dictionary for values of changes in profit/losses
+def pl_change():
+    PyBank_csv_path = os.path.join('Resources','budget_data.csv')
+    with open(PyBank_csv_path,'r') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',')
+        monthlydiff = 0
+        monthlydifftotal = 0
+        previousmonthamt = 0
+        monthlydiffdict = {}
+        for row in csvreader:
+            if row[0] == 'Date':
+                pass
+            else:  #calc for month diff, adding total of diff, add to dictionary, storing previous month info   
+
+                monthlydiff = (int(row[1]) - previousmonthamt)
+                monthlydifftotal = monthlydifftotal + monthlydiff
+                monthlydiffdict.update({str(row[0]) : int(monthlydiff)})
+                previousmonthamt = int(row[1])
+
+    return monthlydiff, monthlydifftotal, monthlydiffdict
+    #return monthlydifftotal
+
+
+#dict.update({'key3':'geeks'})
+
+
+#def for finding average of changes in profit/losses
+#def monthlyave():
+    #PyBank_csv_path = os.path.join('Resources','budget_data.csv')
+    #with open(PyBank_csv_path,'r') as csvfile:
+        #csvreader = csv.reader(csvfile, delimiter=',')
+
+        #monthlydifftotal = 0
+        #previousmonthamt = 0
+
+        #for row in csvreader:
+            #if row[0] == 'Date':
+                #pass
+            #else:  #adding total of diff, storing previous month info   
+
+                #monthlydifftotal = monthlydifftotal + (int(row[1]) - previousmonthamt)
+
+                #previousmonthamt = int(row[1])
+
+    #return monthlydifftotal
+    #monthlydifftotal / (monthcounter - 1)
 
 
 
-#csv_header = next(csvfile)
-#print(f"Header: {csv_header}")
+#test if working
+monthcounter = month_number()
+netamount = net_total()
+monthlydifftotal = pl_change()
+monthlydiff = pl_change()
 
-#added way to print list of headers with number of columns
-#count = 0
-#header_list = csv_header.split(",")
-#for col in header_list:
-#    count +=1
- #   print(count,col)
+monthlydiffdict = pl_change()
 
-# Read the header row first (skip this step if there is not header)
-#csv_header = next(csvfile)
-#print(f"CSV Header: {csv_header}")
+print(monthcounter)
+print(netamount)
+print(monthlydiff)
+print(monthlydifftotal)
+#print(monthlydiffdict)
 
-# Read each row of data after the header
-#for row in csvreader:
- #   print(row)
+
+
+
+
+
+
+
+
+
